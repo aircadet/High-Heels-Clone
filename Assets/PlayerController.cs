58440bd857ffc51e4190785aaa2aed6e613e57e4
Mertcan <aircadetxy@gmail.com>
2021-10-06 16:43:06 +0300

    Project Created

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   [SerializeField] private VariableJoystick joystick;
+   [SerializeField] private float forwardSpeed;
+   [SerializeField] private float sideSpeed;
+   
+}
